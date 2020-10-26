c7122cc38394cc86a57888ffb0099e166d4ae06d
Pacal2 <i-jackie@hotmail.com>
2020-10-26 20:23:05 +0100

    Initial commit

---
+++ HelloWThread/main.cpp
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+
+#include <iostream>
+
+#define MAX_THREAD_CNT 10
+
+int threadCount;
+
+void* threadFunc(void* args);
+
+int main(int argc, char** argv) {
+	
+	long thread;
+	pthread_t threadHandles[MAX_THREAD_CNT];
+	
+	// Ustalenie liczby w¹tków
+	if (argv[1]) {
+		threadCount = strtol(argv[1], NULL, 10);
+	} else {
+		threadCount = MAX_THREAD_CNT;
+	}
+	
+	// Tworzenie w¹tków
+	for (thread = 0; thread < threadCount; thread++){
+		pthread_create(&threadHandles[thread], NULL, threadFunc, (void*)(thread));
+	}
+	
+	// Zakoñczenie pracy
+	for (thread = 0; thread < threadCount; thread++){
+		pthread_join(threadHandles[thread], NULL);
+	}
+	
+	printf("Wszystkie w¹tki zakoñczy³y pracê");
+	
+	return 0;
+}
+
+// funkcja w¹tku
+void* threadFunc(void* args) {
+	
+	//printf("Hello from thread %d\n", (int) args);
+	std::cout << "Hello from thread " << (int) args << std::endl;
+	return NULL;
+	
+}
+
+
+
+
