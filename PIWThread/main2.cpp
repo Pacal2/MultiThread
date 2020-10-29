a0c0392ca6616493582fdcd4be7c2b079234eb2a
Pacal2 <i-jackie@hotmail.com>
2020-10-29 20:12:23 +0100

    mutex

--- PIWThread/main2.cpp
+++ PIWThread/main2.cpp
@@ -3,12 +3,14 @@
 #include <stdlib.h>
 #include <pthread.h>
 
+#define MX_THRREAD_COUNT 20
+
 int thread_count;
 void *Thread_sum(void *rank);
 
 int n;
 double sum;
-volatile int flag;
+pthread_mutex_t mutex;
 
 int main(int argc, char** argv) {
 	
@@ -16,6 +18,8 @@ int main(int argc, char** argv) {
 	thread_count = strtol(argv[1], NULL, 10);
 	sum = 0.0;
 	n = 10000000;
+	
+	pthread_mutex_init(&mutex, NULL);
 	
 	pthread_t thread_handles[thread_count];
 	
@@ -27,14 +31,14 @@ int main(int argc, char** argv) {
 		pthread_join(thread_handles[thread], NULL);
 	}
 	
+	pthread_mutex_destroy;
+	
 	printf("Pi Estimate in %d iterations %d threads is: %.10f\n", n, thread_count, 4*sum);
 	
 	return 0;
 	
 }
 
-#pragma GCC push_options
-#pragma GCC optimize ("O0")
 void *Thread_sum(void *rank) {
 	
 	long my_rank = (long) rank;
@@ -44,23 +48,18 @@ void *Thread_sum(void *rank) {
 	long long first = loc_n * my_rank;
 	long long last = first + loc_n;
 	
-	//factor = (first % 2 == 0 ? 1.0 : -1.0);
-	if (first % 2 == 0) {
-		factor = 1.0;
-	} else {
-		factor = 1.0;
-	}
-	
+	factor = (first % 2 == 0 ? 1.0 : -1.0);
 	
 	for(i=first; i<last; i++, factor = -factor) {
-		
-		while (flag != my_rank);
+		pthread_mutex_lock(&mutex);
 		sum += factor/(2 * i + 1);
-		flag = (flag + 1) % thread_count;
+		pthread_mutex_unlock(&mutex);
 	}
 	
 	return NULL;
 	
 }
-#pragma GCC pop_options
+
+
+
 
